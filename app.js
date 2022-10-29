5d9f52ce7c7bc68008aac3e36185e98b0af99108
Bhuvan Bakshi <bhuvanb@webmob.tech>
2022-10-30 00:43:20 +0530

    add pug

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const path = require("path")
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const adminRoutes = require("./routes/admin")
-const shopRoutes = require("./routes/shop");
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
-const rootDir = require("./utils/path");
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-// serve content statically!
-app.use(express.static(path.join(rootDir,"public")));
-app.use("/admin",adminRoutes);
-app.use(shopRoutes)
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
 
-app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(rootDir,"views","404.html"));
-})
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
+
 app.listen(3000);
