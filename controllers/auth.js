1ac6f844323e239d06cac711e2a6ea5a1d772d93
Bhuvan Bakshi <bhuvanb@webmob.tech>
2022-11-05 13:43:44 +0530

    setup for authentication updated code in case of any issues with auth.js in last branch copy this file to that branch

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,28 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: false
+  const isLoggedIn =
+    req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
-    })
-    .catch(err => console.log(err));
+  User.findById("63654329a44f58602f567314").then((user) => {
+    req.session.isLoggedIn = true;
+    req.session.user = user;
+    req.session.save(err=> {
+      console.log(err);
+      res.redirect("/");
+    }); 
+  }).catch(err => {console.log(err)});
 };
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy(err => {
-    console.log(err);
-    res.redirect('/');
+  req.session.destroy(() => {
+    res.redirect("/");
   });
 };
